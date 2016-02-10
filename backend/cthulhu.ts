ded5dfd8f41941438779659f5a1c9d2a5e9fc4cb
Mika Turunen <mika.turunen@ymail.com>
2016-02-10 20:52:52 +0200

    Hello, World! for express typescript

---
+++ backend/cthulhu.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (request: express.Request, response: express.Response) => {
+    response.send("Hello, World!");
+});
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000!");
+});
