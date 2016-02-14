bb77cde2fd790c4d7fce75ed9078b9403c9a7beb
Mika Turunen <mika.turunen@ymail.com>
2016-02-14 13:45:56 +0200

    Remove bower install from package.json and add static directory to express.

--- backend/cthulhu.ts
+++ backend/cthulhu.ts
@@ -4,10 +4,13 @@
 require("babel-polyfill");
 
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
+// NOTE this is required by Heroku. Heroku heavily relies on environmental variables like this.
 app.set("port", (process.env.PORT || 3000));
+app.use("/components", express.static(path.join(__dirname + "../components")));
 
 app.get("/", (request: express.Request, response: express.Response) => {
     response.send("Hello, World!");
--- release/backend/cthulhu.js
+++ release/backend/cthulhu.js
@@ -4,12 +4,20 @@ var _express = require("express");
 
 var _express2 = _interopRequireDefault(_express);
 
+var _path = require("path");
+
+var path = _interopRequireWildcard(_path);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 require("babel-polyfill");
 
 var app = (0, _express2.default)();
+
 app.set("port", process.env.PORT || 3000);
+app.use("/components", _express2.default.static(path.join(__dirname + "../components")));
 app.get("/", function (request, response) {
     response.send("Hello, World!");
 });
