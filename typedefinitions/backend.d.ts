e20ad4ba31d03cd6538b99af51b4c1263863d19f
Mika Turunen <mika.turunen@ymail.com>
2016-02-14 13:25:41 +0200

    Update dependencies and release chaining a bit.

--- typedefinitions/backend.d.ts
+++ typedefinitions/backend.d.ts
@@ -1,8 +1,9 @@
 
-
+/// <reference path="./node/node.d.ts" />
 /// <reference path="./express/express.d.ts" />
+/// <reference path="./q/Q.d.ts" />
 
 // Reference the files from cthulhu character generator to get the Types from there
 // NOTE currently we get Q, Node.d.ts and cthulhu.d.ts from there -- might be reasonable to hoist
 // them into a better more centralized location but for sake of making things work; this will do.
-/// <reference path="../node_modules/cthulhu-character-generator/typedefinitions/backend.d.ts" />
+/// <reference path="../node_modules/cthulhu-character-generator/typedefinitions/cthulhu.d.ts" />
