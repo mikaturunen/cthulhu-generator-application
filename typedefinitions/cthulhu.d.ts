175a4251130700a694a64f3e4bc10d93af13510d
vintigr <teartiggy@gmail.com>
2016-02-16 18:51:55 +0200

    Added character skills typedefs.

--- typedefinitions/cthulhu.d.ts
+++ typedefinitions/cthulhu.d.ts
@@ -10,57 +10,123 @@ interface Dice {
 	* How many die are being thrown.
 	* For example: 2d6+1 -> count: 2.
 	*/
-	count: number;
+    count: number;
 
 	/**
 	* The max value of the die, or faces.
 	* For example: 2d6+1 -> max: 6.
 	*/
-	max: number;
+    max: number;
 
 	/**
 	* What value gets added to the die total.
 	* For example: 2d6+1 -> add: 1.
 	*/
-	add: number;
+    add: number;
 }
 
 interface Character {
 	/**
 	* Character age, fairly obvious.
 	*/
-	age: number;
+    age: number;
 
 	/**
 	* Male or Female but we are not limited by this constrain; thus, string. Be whoever you want to be! EMBRACE IT!
 	*/
-	gender: string;
+    gender: string;
 
-	sexualOrientation: string;
-	firstName: string;
-	lastName: string;
-	occupation: string;
-	education: string;
-	stats: CharacterStats;
-	}
+    sexualOrientation: string;
+    firstName: string;
+    lastName: string;
+    occupation: string;
+    education: string;
+    stats: CharacterStats;
+	skills: CharacterSkills1920s;
+}
 
-	interface CharacterStats {
-	ageModifier: number;
-	strength: number;
-	dexterity: number;
-	intelligence: number;
-	constitution: number;
-	appearance: number;
-	power: number;
-	size: number;
-	education: number;
-	sanity: number;
-	idea: number;
-	luck: number;
-	knowledge: number;
-	}
+interface CharacterStats {
+    ageModifier: number;
+    strength: number;
+    dexterity: number;
+    intelligence: number;
+    constitution: number;
+    appearance: number;
+    power: number;
+    size: number;
+    education: number;
+    sanity: number;
+    idea: number;
+    luck: number;
+    knowledge: number;
+}
 
-	interface EducationLevel {
-	value: number;
-	level: string;
+interface CharacterSkills {
+
+}
+
+interface CharacterSkills1920s extends CharacterSkills {
+	accounting: number;
+	anthropology: number;
+	archaelogy: number;
+	arts: CustomSkill[];
+	astronomy: number;
+	bargain: number;
+	biology: number;
+	chemistry: number;
+	climb: number;
+	conceal: number;
+	crafts: CustomSkill[];
+	creditRating: number;
+	cthulhuMythos: number;
+	disguise: number;
+	dodge: number;
+	driveAuto: number;
+	electricalRepair: number;
+	fastTalk: number;
+	geology: number;
+	hide: number;
+	history: number;
+	jump: number;
+	law: number;
+	libraryUse: number;
+	listen: number;
+	locksmith: number;
+	martialArts: number;
+	mechanicalRepair: number;
+	medicine: number;
+	naturalHistory: number;
+	navigate: number;
+	occult: number;
+	operateHeavyMachinery: number;
+	otherLanguages: CustomSkill[];
+	nativeLanguages: CustomSkill[];
+	persuade: number;
+	pharmacy: number;
+	photography: number;
+	physics: number;
+	pilot: number;
+	psychoanalysis: number;
+	psychology: number;
+	ride: number;
+	sneak: number;
+	swim: number;
+	throw: number;
+	track: number;
+	customSkills: CustomSkill[];
+	handgun: number;
+	machineGun: number;
+	rifle: number;
+	shotgun: number;
+	smg: number;
+}
+
+interface CustomSkill {
+	name: string;
+	value: string;
+}
+
+interface EducationLevel {
+    value: number;
+    level: string;
 }
