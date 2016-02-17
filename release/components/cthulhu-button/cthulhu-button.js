51f1cf1fe484d17d8bd5ef6b6faf6af66933948d
Mika Turunen <mika.turunen@cybercom.com>
2016-02-17 12:20:45 +0200

    Create cthulhu-button
    Base button created

---
+++ release/components/cthulhu-button/cthulhu-button.js
@@ -0,0 +1,10 @@
+"use strict";
+
+define("cthulhuButton", [], function () {
+    Polymer({
+        is: "cthulhu-button",
+        properties: {
+            isPrimary: Boolean
+        }
+    });
+});
