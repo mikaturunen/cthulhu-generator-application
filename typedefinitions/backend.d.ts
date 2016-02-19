0ee42a615e59711825927e7ce70ea968e78fb697
Mika Turunen <mika.turunen@ymail.com>
2016-02-19 11:47:49 +0200

    #1 add dozer or so missing typedefinitions for the middlewares used

--- typedefinitions/backend.d.ts
+++ typedefinitions/backend.d.ts
@@ -1,4 +1,10 @@
 
 /// <reference path="./node/node.d.ts" />
 /// <reference path="./express/express.d.ts" />
+/// <reference path="./express-session/express-session.d.ts" />
+/// <reference path="./passport/passport.d.ts" />
+/// <reference path="./passport-strategy/passport-strategy.d.ts" />
+/// <reference path="./passport-google-oauth/passport-google-oauth.d.ts" />
+/// <reference path="./body-parser/body-parser.d.ts" />
+/// <reference path="./cookie-parser/cookie-parser.d.ts" />
 /// <reference path="./q/Q.d.ts" />
