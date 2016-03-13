d3499e011eb1771a44b62ed285fb303e3d8cec66
Mika Turunen <mika.turunen@ymail.com>
2016-03-13 14:44:40 +0200

    Threw togehter random header navigation with butt ugly defaults.

--- components/cthulhu-header/cthulhu-header.ts
+++ components/cthulhu-header/cthulhu-header.ts
@@ -3,7 +3,17 @@
 define("cthulhuHeader", [], () => {
 	Polymer({
 		is: "cthulhu-header",
+
 		properties: {
+		},
+
+		/**
+		 * Handles the user logging out of the application.
+		 */
+		handleLogout: function() {
+			// We also skip over the single page application and force a refresh to make sure
+			// the cookie gets cleared from the browser once the backend tells it to do so. Refresh guarantees this.
+			window.location.href = "/location";
 		}
 	});
 });
