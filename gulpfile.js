2e074de40b5e5cae40f60b5f84435b449dafe092
Mika Turunen <mika.turunen@ymail.com>
2016-03-28 12:15:23 +0300

    Start using the shared build chain for javascript from javascript-build-essentials toolkit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,16 @@
 "use strict";
 
-/**
- * Gulp usage file for the whole project.
- */
+const gulp = require("gulp");
+const sequence = require("run-sequence").use(gulp);
+const buildSteps = require("./javascript-build-essentials/build/shared-gulpfile");
 
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var eventStream = require("event-stream");
-var tslint = require("gulp-tslint");
-var sequence = require("run-sequence").use(gulp);
-var babel = require("gulp-babel");
-var path = require("path");
-var jade = require("gulp-jade");
-var notify = require("gulp-notify");
-var less = require("gulp-less");
-var combiner = require("stream-combiner2");
+const outputDirectory = "./release/";
 
-var typeScriptDestination = "./release/";
+gulp.task("ts-back", () => buildSteps.createTypeScriptTask([ "./backend/**/*.ts", "./typedefinitions/backend.d.ts" ], outputDirectory));
+gulp.task("ts-front", () => buildSteps.createTypeScriptTask([ "./components/**/*.ts", "./typedefinitions/frontend.d.ts" ], outputDirectory));
+gulp.task("jade", () => buildSteps.createJadeTask([ "./components/**/*.jade" ], outputDirectory));
+gulp.task("less", () => buildSteps.createLessTask([ "./components/**/*.less" ], outputDirectory));
 
-// Used to stop the 'watch' behavior from breaking on emited errors, errors should stop the process
-// in all other cases but 'watch' as 'watch' is ongoing, iterating, always on process :)
-var globalEmit = false;
-
-/**
- * Creates TypeScript compilation for given sources files and outputs them into a preferred release location.
- * Used for frontend and backend TypeScript. They need different compilation locations.
- * @param {String[]} typescriptSources Array of source files
- * @param {String} outputDirectory Location to output the JS files to.
- * @returns {Object} Gulp stream.
- */
-var createTypeScriptTaskForTargets = function(typescriptSources, outputDirectory) {
-    var tsOptions = {
-        typescript: require("typescript"),
-        target: "es6",
-        sourceMap: true,
-        removeComments: false,
-        declaration: true,
-        noImplicitAny: true,
-        module: "es2015",
-        failOnTypeErrors: true,
-        suppressImplicitAnyIndexErrors: true
-    };
-
-    return gulp
-        .src(typescriptSources)
-        // Pipe source to lint
-        .pipe(tslint())
-        .pipe(tslint.report("verbose", { emitError: globalEmit }))
-        // Push through to compiler
-        .pipe(ts(tsOptions))
-        // Through babel (es6->es5)
-        .pipe(babel())
-        .pipe(gulp.dest(outputDirectory));
-};
-
-// TYPESCRIPT COMPILATION
-gulp.task("ts-back", function() {
-    return createTypeScriptTaskForTargets([
-            "./typedefinitions/backend.d.ts",
-            "./typedefinitions/cthulhu.d.ts",
-            "./backend/**/*.ts"
-        ],
-        path.join(typeScriptDestination, "/backend")
-    );
-});
-
-// Frontend compilation
-gulp.task("ts-front", function() {
-    return createTypeScriptTaskForTargets([
-            "./typedefinitions/frontend.d.ts",
-            "./typedefinitions/cthulhu.d.ts",
-            "./components/**/*.ts"
-        ],
-        path.join(typeScriptDestination, "/components")
-    );
-});
-
-// JADE COMPILATION
-gulp.task("jade", function() {
-    var j = jade({ pretty: true });
-
-    if(globalEmit === false) {
-        j.on('error', notify.onError(function (error) {
-            return 'An error occurred while compiling Jade.\nLook in the console for details.\n' + error;
-        }));
-    }
-
-    return gulp.src("./components/**/*.jade")
-        .pipe(j)
-        .pipe(gulp.dest("./release/components"));
-});
-
-gulp.task("less", function() {
-    var combined = combiner.obj([
-        gulp.src("./components/**/*.less"),
-        less(),
-        gulp.dest("./release/components")
-    ]);
-
-    if (globalEmit === false) {
-        combined.on("error", notify.onError(function (error) {
-            return 'An error occurred while compiling Less.\nLook in the console for details.\n' + error;
-        }))
-    }
-
-    return combined;
-});
 
 /**
  * Run with: 'gulp w'
@@ -147,17 +52,12 @@ gulp.task("w", function() {
     );
 });
 
-/**
- * Run with: 'gulp' or 'gulp default'
- */
-gulp.task("default", function() {
-    globalEmit = true;
-    return sequence(
-        [
-            "ts-back",
-            "ts-front",
-            "jade",
-            "less"
-        ]
-    );
+gulp.task("default", () => {
+    buildSteps.globalEmitOn();
+    return sequence([
+		"ts-back",
+		"ts-front",
+		"jade",
+		"less"
+	]);
 });
